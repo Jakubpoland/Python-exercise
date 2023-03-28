b7140e0a151227da193efa0ea6a14b45990b3899
Jakubpoland <103152998+Jakubpoland@users.noreply.github.com>
2023-03-28 22:41:46 +0200

    Add files via upload

---
+++ for_excercise.py
@@ -0,0 +1,35 @@
+#Parzuste od 1 do 10
+
+"""
+for i in range (1,10):
+    if(i%2):
+        print(i)
+"""
+
+
+#Co 3 od 1 do 100
+
+"""
+for i in range (1,100,3):
+    print(i)
+"""
+
+
+#Najmniejsza z listy
+
+"""
+lista = [1,3,7,11,2,-6,0]
+najmniejsza = None
+for i in lista:
+    if najmniejsza == None or najmniejsza > i:
+        najmniejsza = i
+print(najmniejsza)
+"""
+
+#Wypisywanie sumy porzedniej i obecnej liczby
+"""
+poprzednia = 0
+for i in range (1,10):
+    print(poprzednia+i)
+    poprzednia = i
+""" 
