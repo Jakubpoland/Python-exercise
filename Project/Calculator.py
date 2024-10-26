70a23fbd9bfd60fcaa96cdd57c5d8417210ba26e
Jakubpoland <103152998+Jakubpoland@users.noreply.github.com>
2024-10-26 12:04:48 +0200

    Visual changes

--- Project/Calculator.py
+++ Project/Calculator.py
@@ -15,14 +15,21 @@
 
 
 
-from tkinter import * #Import biblioteki
+from tkinter import *  #Import biblioteki
+
 
 root = Tk()
 root.title("Kalkulator") #Nazwanie okienka
+root.geometry("700x500")
 
-e = Entry(root, width=45, borderwidth=5, relief='solid')  #Ustalenie wyglądu okienka wpisu
-b = Entry(root, width=45, borderwidth=5)
-e.grid(row=0, column=0, columnspan=3, padx=10, pady=10) #Ustalenie wyglądy pola klawiszy
+e = Entry(root, width=40, borderwidth=5, font=('Arial', 18), justify='right')  #Ustalenie wyglądu okienka wpisu
+e.grid(row=0, column=0, columnspan=4, padx=10, pady=10) 
+
+history_entry = Entry(root, width=40, font=('Arial', 18), justify='right') #Ustalenie wyglądu histori
+history_entry.grid(row=1, column=0, columnspan=4, padx=10, pady=10)
+
+history_display = Text(root, width=30, height=20, font=('Arial', 12), state=NORMAL) #Ustalenie wyglądu histori po prawej stronie
+history_display.grid(row=0, column=4, rowspan=6, padx=10, pady=10)
 
 def button_click(number): #Funkcja odpowiadająca za wpisywanie liczb
     #e.delete(0, END)
@@ -70,35 +77,35 @@ button_dot = Button(root, text=".", padx=40, pady=20)
 #Zainicjowanie przycisków i ich wyglądu
 #
 
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
-button_division.grid(row=1, column=3)
+button_7.grid(row=2, column=0)
+button_8.grid(row=2, column=1)
+button_9.grid(row=2, column=2)
+button_division.grid(row=2, column=3)
 #1 rząd przycisków
 
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
-button_multiplication.grid(row=2, column=3)
+button_4.grid(row=3, column=0)
+button_5.grid(row=3, column=1)
+button_6.grid(row=3, column=2)
+button_multiplication.grid(row=3, column=3)
 #2 rząd przycisków
 
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
-button_subtraction.grid(row=3, column=3)
+button_1.grid(row=4, column=0)
+button_2.grid(row=4, column=1)
+button_3.grid(row=4, column=2)
+button_subtraction.grid(row=4, column=3)
 #3 rząd przycisków
 
 
-button_clear.grid(row=4, column=0)
-button_0.grid(row=4, column=1)
-button_dot.grid(row=4, column=2)
-button_addition.grid(row=4, column=3)
+button_clear.grid(row=5, column=0)
+button_0.grid(row=5, column=1)
+button_dot.grid(row=5, column=2)
+button_addition.grid(row=5, column=3)
 
 #4 rząd przycisków
 
 
 
-button_equation.grid(row=5, column=0, columnspan=4)
+button_equation.grid(row=6, column=0, columnspan=4)
 
 
 # 
